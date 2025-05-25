79d1a6ea7e4371cb01317dcbc24be144c66bef42
muhammadhadi-dotnet <hadikhandev364@gmail.com>
2025-05-25 18:49:18 +0500

    inital commiit

---
+++ mvc-architecture/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvc_architecture.Models;
+
+namespace mvc_architecture.Data
+{
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<User> users { get; set; }
+            // Add your DbSets here
+        }
+    
+}
---
+++ mvc-architecture/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mvc_architecture.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Email { get; set; } = null;
+        public int Age { get; set; }
+        public string? Address { get; set; }
+
+    }
+}
